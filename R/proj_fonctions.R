25a1f2baa489d3126d05362810d52af06a24d17a
Ismaila <ba.ismaila@courrier.uqam.ca>
2019-05-14 22:40:55 -0400

    troisieme commit

--- R/proj_fonctions.R
+++ R/proj_fonctions.R
@@ -186,7 +186,4 @@ Estimate.Properties <- function(Est.theta, init.theta) {
   return(list(Bias=Bias.est, Var=Var.est, MSE=MSE.est, FPR=FPR.est, TPR=TPR.est))
 }
 
-## Theta initial for scenario 1 and 2
-Theta.Init.Strauss <- c(beta0.sc12,2,0.75,rep(0,48),log(0.5))
-Theta.Init.Poisson <- c(beta0.sc12,2,0.75,rep(0,48),log(1))
-Theta.Init.Geyer <- c(beta0.sc12,2,0.75,rep(0,48),log(1.5))
+
