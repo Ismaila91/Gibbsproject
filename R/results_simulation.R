808ae081f5ad36fd1a316691d2000a0c7f34da18
Ismaila <ba.ismaila@courrier.uqam.ca>
2019-05-14 22:40:11 -0400

    premier commit

---
+++ R/results_simulation.R
@@ -0,0 +1,4 @@
+## Theta initial for scenario 1 and 2
+Theta.Init.Strauss <- c(beta0.sc12,2,0.75,rep(0,48),log(0.5))
+Theta.Init.Poisson <- c(beta0.sc12,2,0.75,rep(0,48),log(1))
+Theta.Init.Geyer <- c(beta0.sc12,2,0.75,rep(0,48),log(1.5))
